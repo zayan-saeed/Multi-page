0c071169a5d8a2f1388b56d077e80040511657f1
Zayan Saeed <sabithasaeed@gmail.com>
2024-10-19 21:49:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='Multi-page'>
     <App />
   </BrowserRouter>
 );
